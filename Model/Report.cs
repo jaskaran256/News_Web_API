2a9755d3d3c7e44cb891afb66a1390e8a61f3ec5
Jaskaran <singh.jass95@gmail.com>
2019-12-01 21:50:26 +1300

    Add project files.

---
+++ Model/Report.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News_Web_API.Model
+{
+    public class Report
+    {
+        public int Id { get; set; }
+
+        public string NewsHeadline { get; set; }
+
+        public string Detail { get; set; }
+
+        public DateTime Published { get; set; }
+
+
+    }
+}
